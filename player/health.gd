20f02b7812990bc2523ddd24d1f13d4e87a5f18c
Rubycule <robin.trurnit@gmail.com>
2024-01-27 15:13:47 +0100

    generic health changed signal added

--- player/health.gd
+++ player/health.gd
@@ -5,7 +5,8 @@ class_name health
 @export var start_hit_points : int
 var current_hit_points : int
 
-signal depleted
+signal hit_points_depleted
+signal hit_points_changed(new_amount : int)
 
 func _ready():
 	current_hit_points = start_hit_points
@@ -14,9 +15,11 @@ func heal_by_amount(heal_amount : int):
 	current_hit_points += heal_amount
 	if current_hit_points > max_hit_points:
 		current_hit_points = max_hit_points
+	hit_points_changed.emit(current_hit_points)
 
 func heal_fully():
 	current_hit_points = max_hit_points
+	hit_points_changed.emit(current_hit_points)
 
 func take_damage(damage_amount : int):
 	if current_hit_points <= 0:
@@ -24,4 +27,5 @@ func take_damage(damage_amount : int):
 	current_hit_points -= damage_amount
 	if current_hit_points <= 0:
 		current_hit_points = 0
-		depleted.emit()
+		hit_points_depleted.emit()
+	hit_points_changed.emit(current_hit_points)
