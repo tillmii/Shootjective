7bbe2108973d235f65d718c8b1725f6be20572fd
Till <tillsimonmichels@gmail.com>
2024-01-27 19:27:13 +0100

    Fix projectile

---
+++ projectile/projectile.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+var DIRECTION: Vector2 = Vector2.ZERO
+var SPEED = 1000
+var _active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Move
+	var delta_pos = DIRECTION * SPEED
+	position += delta_pos * delta
+	
+	# Detect collision
+	if !_active:
+		return
+	if has_overlapping_bodies():
+		for body in get_overlapping_bodies():
+			if body.get_collision_layer() == 2:
+				print("player")
+			if body.get_collision_layer() == 1:
+				print("wall")
+		queue_free()
+
+
+func apply_impulse(dir: Vector2, speed = SPEED):
+	DIRECTION = dir.normalized()
+	SPEED = speed
+
+
+func _on_body_exited(body):
+	_active = true
